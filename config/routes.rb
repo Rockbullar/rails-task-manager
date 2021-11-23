1f482c67da75718b331b032ddcc996d122a94e66
Stephen Kusrianto <stephenkusrianto@gmail.com>
2021-11-23 12:46:48 +0800

    left with edit update and delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get 'tasks', to:'tasks#index' # dhunn
+  get 'tasks/new', to: 'tasks#new', as: 'new_task' # dhunn
+  get 'tasks/:id', to: 'tasks#show', as: 'task' # dhunn
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/id', to: 'tasks#delete', as: 'delete_task'
 end
-
-
-# As a user, I can list tasks
-# As a user, I can view the details of a task
-# As a user, I can add a new task
-# As a user, I can edit a task (mark as completed / update title & details)
-# As a user, I can remove a task
