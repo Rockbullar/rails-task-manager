3957dd79dacb68c11fc546d0079e6dbc539e6259
Stephen Kusrianto <stephenkusrianto@gmail.com>
2021-11-23 14:23:36 +0800

    completed challenge 2!

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,17 +18,26 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+	  if @task.update(task_params)
+		  redirect_to task_path(@task)
+	  end
   end
 
   def delete
+    task = Task.find(params[:id])
+	  if task.destroy
+		  redirect_to tasks_path
+	  end
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :description)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'tasks', to:'tasks#index' # dhunn
   get 'tasks/new', to: 'tasks#new', as: 'new_task' # dhunn
   get 'tasks/:id', to: 'tasks#show', as: 'task' # dhunn
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/id', to: 'tasks#delete', as: 'delete_task'
+  post 'tasks', to: 'tasks#create' # dhunn
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' #dhunn
+  patch 'tasks/:id', to: 'tasks#update' #dhunn
+  delete 'tasks/:id', to: 'tasks#delete', as: 'delete_task'
 end
